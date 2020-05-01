4ebe82eec614a660c53d2e9807033ee63a9558ed
Nate Rutman <nathan.rutman@gmail.com>
2020-05-01 17:54:08 -0400

    Update homepage redirect

--- app/routes.php
+++ app/routes.php
@@ -1,16 +1,12 @@
 <?php
 declare(strict_types=1);
 
+use App\Application\Actions\Home\ViewHomeAction;
 use App\Application\Actions\Testimony\ViewTestimonyAction;
 use Slim\App;
-use Slim\Psr7\Response;
 
 return function (App $app) {
-    // temporarily redirect home to provchurch website
-    // $app->get('/', ViewHomeAction::class);
-    $app->get('/', function ($request, Response $response) use ($app) {
-        return $response->withStatus(302)->withHeader('Location', 'https://provchurch.org');
-    });
+    $app->get('/', ViewHomeAction::class);
 
     $app->get('/{slug}', ViewTestimonyAction::class);
 };
