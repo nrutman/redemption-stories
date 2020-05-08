4d0127555e152b53f73f6aafe544edba044726ef
Nate Rutman <nathan.rutman@gmail.com>
2020-05-08 13:54:21 -0400

    Redirect stories to /story/

--- app/routes.php
+++ app/routes.php
@@ -3,10 +3,17 @@ declare(strict_types=1);
 
 use App\Application\Actions\Home\ViewHomeAction;
 use App\Application\Actions\Testimony\ViewTestimonyAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
 return function (App $app) {
     $app->get('/', ViewHomeAction::class);
 
-    $app->get('/{slug}', ViewTestimonyAction::class);
+    // temporary redirect
+    $app->get('/01-lisa', function (Request $request, Response $response) {
+        return $response->withStatus(302)->withHeader('Location', '/story/01-lisa');
+    });
+
+    $app->get('/story/{slug}', ViewTestimonyAction::class);
 };
